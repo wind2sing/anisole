35de1f1913f2eb48f8a219f0cf11bf8b507608fc
zireael <zireael.me@gmail.com>
2019-08-11 15:10:09 +0800

    feat: bangumi detailed api

--- anisole/bgm/bangumi.py
+++ anisole/bgm/bangumi.py
@@ -3,6 +3,7 @@ import click
 
 from anisole import TOKEN
 from anisole.utils import pformat_list
+from anisole.bgm.auth import check_token
 
 
 API_PREFIX = "https://api.bgm.tv"
@@ -12,13 +13,21 @@ class TokenNotFound(Exception):
     pass
 
 
+class BadAPIRequest(Exception):
+    pass
+
+
 class API:
     """Simple API class to work with bgm.tv"""
 
     def __init__(self, watcher):
+        self.watcher = watcher
+
+    @property
+    def headers(self):
         if not TOKEN:
             raise TokenNotFound
-        self.watcher = watcher
+        return {"Authorization": f'Bearer {TOKEN.get("access_token")}'}
 
     def cal(self, filter_rating_count=10):
         url = f"{API_PREFIX}/calendar"
@@ -38,4 +47,30 @@ class API:
                     index += 1
 
             click.echo(pformat_list(li, align=40))
+
+    def search(self, keyword, typ=2):
+        r = requests.get(f"{API_PREFIX}/search/subject/{keyword}?type={typ}")
+        r.raise_for_status()
+        return r.json()
+
+    def auth(self):
+        return check_token()
+
+    def collection_update(self, subject_id: int, status="do", action="update"):
+        url = f"{API_PREFIX}/collection/{subject_id}/{action}"
+        r = requests.post(url, headers=self.headers, data={"status": status})
+        if r.status_code == 200:
+            return True
+        print(r.text)
+        raise BadAPIRequest(url)
+
+    def watched_until(self, subject_id: int, watched_eps: int):
+        url = f"{API_PREFIX}/subject/{subject_id}/update/watched_eps"
+        r = requests.post(url, headers=self.headers, data={"watched_eps": watched_eps})
+
+        if r.status_code == 200:
+            if r.json()["code"] == 202:
+                return True
+        print(r.text)
+        raise BadAPIRequest(url)
 
