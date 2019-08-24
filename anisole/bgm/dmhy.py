cc0075b57fdc0b42ffce06dafa22991598adb225
zireael <zireael.me@gmail.com>
2019-08-24 20:41:41 +0800

    fix: update dmhy parsing rules

--- anisole/bgm/dmhy.py
+++ anisole/bgm/dmhy.py
@@ -1,6 +1,6 @@
 import re
 
-from acrawler import Crawler, ParselItem, Processors, Request, Response, Task
+from acrawler import Crawler, ParselItem, Processors as x, Request, Response, Task
 
 from anisole.bgm.sub import Sub
 from anisole.utils import is_chs, parse_anime_ep
@@ -8,24 +8,16 @@ from anisole.utils import is_chs, parse_anime_ep
 
 class DMHYLink(ParselItem):
     store = True
-    css_rules = {"title": ".title > a ::text"}
-    css_rules_first = {
-        "tag": ".tag a::text",
-        "link": "a.arrow-magnet::attr(href)",
+    css = {
+        "tag": [".tag a::text", x.strip(), lambda s: "其它" if s is None else s],
+        "link": "a.arrow-magnet::attr(href)",  # lambda s: s.split("&", 1)[0],
         "size": ".title + td + td::text",
-        "sort": "a[href*=sort]::attr(href)",
-    }
-
-    field_processors = {
-        "title": ["".join, Processors.strip],
-        "tag": [Processors.strip, lambda s: "其它" if s is None else s],
-        # "link": lambda s: s.split("&", 1)[0],
-        "sort": lambda s: int(s.rsplit("/", 1)[-1]),
+        "sort": ["a[href*=sort]::attr(href)", lambda s: int(s.rsplit("/", 1)[-1])],
+        "title": ["[.title > a ::text]", "".join, x.strip()],
     }
 
     def custom_process(self, content):
         content["chs"] = is_chs(content["title"])
-
         title = content["title"]
 
         content["episode"] = -2
@@ -50,7 +42,7 @@ class DMHYTask(Task):
         self.team_id = team_id
         self.order = order
         self.urls = []
-        for page in range(1, 4):
+        for page in range(1, 6):
             url = "https://share.dmhy.org/topics/list/page/{}?keyword={}&sort_id={}&team_id={}&order={}".format(
                 page, self.sub.keyword, self.sort_id, self.team_id, self.order
             )
@@ -66,4 +58,4 @@ class DMHYTask(Task):
 
 
 class DMHYCrawler(Crawler):
-    pass
+    request_config = {"timeout": 4}
